71f53d99855292851ecc327009690c2516f2982f
samailamalima <samuelmalima09@gmail.com>
2023-08-03 13:38:20 +0100

    add

--- projects2/frontend/test/Malima.js
+++ projects2/frontend/test/Malima.js
@@ -9,7 +9,7 @@ describe("MalimaToken", function () {
   beforeEach(async function () {
     [owner, addr1] = await ethers.getSigners();
 
-    const malimaContract = await ethers.getContractFactory("EPIToken");
+    const malimaContract = await ethers.getContractFactory("MalimaToken");
     Malima = await malimaContract.deploy();
 
     // Mint some tokens to the contract creator
@@ -17,8 +17,8 @@ describe("MalimaToken", function () {
   });
 
   it("Should return the correct name, symbol, and total supply", async function () {
-    expect(await Malima.name()).to.equal("Eth Proof Intermediate Token");
-    expect(await Malima.symbol()).to.equal("EPI");
+    expect(await Malima.name()).to.equal("Eth MalimaToken");
+    expect(await Malima.symbol()).to.equal("MLT");
     expect(await Malima.totalSupply()).to.equal(12);
   });
 
@@ -37,7 +37,7 @@ describe("MalimaToken", function () {
   });
 
   it("Should revert if an invalid address is provided to mint", async function () {
-    await expect(Malima.connect(owner).mint("0x2546BcD3c84621e976D8185a91A922aE77ECEc30", 1)).to.be.revertedWith("Invalid address");
+    await expect(Malima.connect(owner).mint("0x5FbDB2315678afecb367f032d93F642f64180aa3", 1)).to.be.revertedWith("Invalid address");
   });
 
   it("Should revert if the contract creator doesn't have sufficient balance to burn", async function () {
